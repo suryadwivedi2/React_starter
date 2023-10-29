9eb872b12a6a7d7aea7a781dd94f5f1122cee21c
suryadwivedi2 <bcae2.089244020218@gmail.com>
2023-10-29 18:23:37 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './componets/ExpenseItem'
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/ExpenseItem.js
@@ -0,0 +1,14 @@
+//import Expense 
+
+function ExpenseItem(){
+    return (
+        <div>
+            <h1>Expense Item</h1>
+            <div>food rs 10</div>
+            <div>petrol rs100</div>
+            <div>movie rs 200</div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
