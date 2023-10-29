67b0e07035d388e390763deb3458b27352708761
suryadwivedi2 <bcae2.089244020218@gmail.com>
2023-10-29 18:31:34 +0530

    v1

--- src/componets/ExpenseItem.js
+++ src/componets/ExpenseItem.js
@@ -1,14 +1,17 @@
-//import Expense 
+import './ExpenseItem.css'
 
-function ExpenseItem(){
+function ExpenseItem() {
     return (
-        <div>
-            <h1>Expense Item</h1>
-            <div>food rs 10</div>
-            <div>petrol rs100</div>
+        <div className="expense-item">
+            <h2>Expense Item</h2>
+            <div className='expense-item__description'><h2>food rs 10</h2></div>
+            <div className="expense-item__price">petrol rs100</div>
             <div>movie rs 200</div>
         </div>
     )
 }
 
 export default ExpenseItem;
+
+
+//we type className in jsx as comapred to html where we write class only
